2640b9df17226741c9352f558bf0fde949ecd379
Documenter.jl <documenter@juliadocs.github.io>
2026-07-16 19:39:27 +0000

    build based on 5837479

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#GlobalOpt","page":"Home","title":"GlobalOpt","text":"Documentation for GlobalOpt.\n\n","category":"section"},{"location":"#GlobalOpt.optimize-Union{Tuple{T}, Tuple{Function, Vector{Float64}, Vector{Float64}}} where T<:GlobalOpt.AbstractOptimizer","page":"Home","title":"GlobalOpt.optimize","text":"Solve  min f(x) s.t.,  lb ≤ x ≤ ub        g(x) ≤ 0        h(x) = 0 The function 'f' must return     f, g, h = f(x)\n\n\n\n\n\n","category":"method"}]
+[{"category":"section","location":"#GlobalOpt","page":"Home","text":"Documentation for GlobalOpt.\n\n","title":"GlobalOpt"},{"category":"method","location":"#GlobalOpt.optimize-Union{Tuple{F}, Tuple{T}, Tuple{Function, Vector{Float64}, Vector{Float64}}} where {T<:GlobalOpt.AbstractOptimizer, F}","page":"Home","text":"Solve  min f(x) s.t.,  lb ≤ x ≤ ub        g(x) ≤ 0        h(x) = 0 The function 'f' must return     f, g, h = f(x)\n\n\n\n\n\n","title":"GlobalOpt.optimize"}]
 }
